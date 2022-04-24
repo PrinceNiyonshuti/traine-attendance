d3438abcf61ac697ffa27434e6292adc98074b55
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-24 20:37:16 +0200

    ft(Trainee): added trainee model , factory , seed and resources

---
+++ app/Http/Controllers/TraineeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trainee;
+use Illuminate\Http\Request;
+
+class TraineeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trainee  $trainee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trainee $trainee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Trainee  $trainee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trainee $trainee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Trainee  $trainee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trainee $trainee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trainee  $trainee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trainee $trainee)
+    {
+        //
+    }
+}
