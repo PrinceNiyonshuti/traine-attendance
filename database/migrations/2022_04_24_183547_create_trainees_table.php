d3b307362e77e0c9480359145d44c0e713ede13e
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-25 23:53:19 +0200

    ft(Migration): made the migration values for trainee

--- database/migrations/2022_04_24_183547_create_trainees_table.php
+++ database/migrations/2022_04_24_183547_create_trainees_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('trainees', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('fistName');
+            $table->text('lastName');
+            $table->text('email');
+            $table->text('phone');
+            $table->longText('bio');
+            $table->text('profile');
         });
     }
 
