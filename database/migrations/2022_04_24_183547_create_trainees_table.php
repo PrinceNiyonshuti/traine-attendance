17f50e515ae560a6a0664b9a57190ccfb5ee1006
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-28 21:43:48 +0200

    ft(folder structure): defined app folder structure

--- database/migrations/2022_04_24_183547_create_trainees_table.php
+++ database/migrations/2022_04_24_183547_create_trainees_table.php
@@ -15,13 +15,14 @@ return new class extends Migration
     {
         Schema::create('trainees', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            $table->text('fistName');
+            $table->text('firstName');
             $table->text('lastName');
             $table->text('email');
             $table->text('phone');
             $table->longText('bio');
             $table->text('profile');
+            $table->timestamps();
+
         });
     }
 
