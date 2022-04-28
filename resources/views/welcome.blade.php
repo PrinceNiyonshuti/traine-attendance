31bb85202e1c14b3ce68181c16e8e7c53153eade
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-28 22:14:28 +0200

    ft(login): implemented login view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,7 +8,6 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
@@ -17,7 +15,6 @@
     <link rel="stylesheet" href="{{asset('assets/css/tailwind.output.css')}}" />
     <script src="{{asset('assets/js/charts-lines.js')}}" defer></script>
     <script src="{{asset('assets/js/charts-pie.js')}}" defer></script>
-    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body>
