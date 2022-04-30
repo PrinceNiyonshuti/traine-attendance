bcd1c2a17f29520d327cc74cb8d3a84758f185ae
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-30 23:04:39 +0200

    rf(Forgot): added go back anchor

--- resources/views/forgot.blade.php
+++ resources/views/forgot.blade.php
@@ -40,10 +40,18 @@
                         <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="./login.html">
                             Recover password
                         </a>
+                        <hr class="my-8" />
+
+                        <p class="mt-4">
+                            <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="/">
+                                Go back to login
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
