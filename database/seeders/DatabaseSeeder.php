66263fc145632bb89277e69e85e6022da7f4e7ff
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-09 23:30:25 +0200

    ch(Leaves): added leaves or permission seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(1)->create();
         \App\Models\Trainee::factory(10)->create();
+        \App\Models\Leave::factory(10)->create();
     }
 }
