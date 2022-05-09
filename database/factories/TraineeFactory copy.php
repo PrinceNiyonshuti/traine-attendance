ab79cfdb5180cdc5ac4b18aa8b145161ffb01bee
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-09 23:38:22 +0200

    ch(Networks): added networks factory data

---
+++ database/factories/TraineeFactory copy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Trainee>
+ */
+class TraineeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'networkName' => $this->faker->lastName(),
+            'networkAddress' => $this->faker->ipv4(),
+            'networkStatus' => $this->faker->boolean(),
+        ];
+    }
+}
