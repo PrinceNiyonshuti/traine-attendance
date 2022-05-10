795e43bdcd0dc17744ed0d068ddd765d36180057
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-10 21:29:52 +0200

    ch(Register): connected register to mentor dashboard

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -57,7 +57,7 @@
                         </div>
 
                         <!-- You should use a button here, as the anchor is only used for the example  -->
-                        <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="./login.html">
+                        <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
                             Create account
                         </a>
                         <hr class="my-8" />
