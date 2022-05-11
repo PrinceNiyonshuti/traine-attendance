3619a5add0a28e7a8ce10d4010efed7e40b8aa1f
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-11 21:06:23 +0200

    ch(layout): adden notification link to layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -72,6 +72,14 @@
                             <span class="ml-4">Settings</span>
                         </a>
                     </li>
+                    <li class="relative px-6 py-3">
+                        <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="buttons.html">
+                            <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
+                            </svg>
+                            <span class="ml-4">Notifications</span>
+                        </a>
+                    </li>
                 </ul>
                 <div class="px-6 my-6">
                     <button class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
