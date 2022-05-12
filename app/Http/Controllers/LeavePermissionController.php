a8a4e3d26665da0e29a4059506d0cc811b679d5c
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-12 09:48:32 +0200

    ft(Leave Permission) : added model , migration , factory and resource

---
+++ app/Http/Controllers/LeavePermissionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LeavePermission;
+use Illuminate\Http\Request;
+
+class LeavePermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LeavePermission  $leavePermission
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LeavePermission $leavePermission)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LeavePermission  $leavePermission
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LeavePermission $leavePermission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LeavePermission  $leavePermission
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LeavePermission $leavePermission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LeavePermission  $leavePermission
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LeavePermission $leavePermission)
+    {
+        //
+    }
+}
