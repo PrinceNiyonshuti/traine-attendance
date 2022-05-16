1e053220485b2f1b521f8c83286b04d49bfce5f5
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-16 09:36:23 +0200

    ch(Mentor): styled notification page

---
+++ resources/views/mentor/notifications.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <div class="container px-6 mx-auto grid">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Notifications
+        </h2>
+    </div>
+</x-layout>
