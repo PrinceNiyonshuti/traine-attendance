5d09182a676252969af1de03247748acfc28b400
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-18 18:35:53 +0200

    ft(Register): form added cross site reference "

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -22,6 +22,7 @@
     <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
         <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
             <form>
+                @csrf
                 <div class="flex flex-col overflow-y-auto md:flex-row">
                     <div class="h-32 md:h-auto md:w-1/2">
                         <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../assets/img/create-account-office.jpeg" alt="Office" />
