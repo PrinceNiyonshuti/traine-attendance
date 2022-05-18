651a34c14c064a8b4fd7ea9b405774b0825e1339
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-18 18:43:42 +0200

    ft(notifications): updated leave permission notifications dropdown

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -207,7 +207,7 @@
                                 <ul x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click.away="closeNotificationsMenu" @keydown.escape="closeNotificationsMenu" class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700">
                                     <li class="flex">
                                         <a class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200" href="#">
-                                            <span>Messages</span>
+                                            <span>Leave Permissions</span>
                                             <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
                                                 13
                                             </span>
