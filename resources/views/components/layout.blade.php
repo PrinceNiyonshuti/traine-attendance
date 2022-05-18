06a145dbd6609afba91ae6f981aba3c75c6db196
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-18 18:45:30 +0200

    ft(notifications): updated logs notifications dropdown

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -223,7 +223,7 @@
                                     </li>
                                     <li class="flex">
                                         <a class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200" href="#">
-                                            <span>Alerts</span>
+                                            <span>Logs</span>
                                         </a>
                                     </li>
                                 </ul>
