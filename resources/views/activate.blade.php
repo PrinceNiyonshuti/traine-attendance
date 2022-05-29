637a61aef6be7d51a1086180543193cf6764c04d
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-29 17:38:31 +0200

    ft(Activate): made the design and layout

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -31,19 +31,9 @@
                         <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                             Activate Account
                         </h1>
-                        <label class="block text-sm">
-                            <span class="text-gray-700 dark:text-gray-400">New Password</span>
-                            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="New Password" />
-                        </label>
-
-                        <label class="block text-sm">
-                            <span class="text-gray-700 dark:text-gray-400">Confirm Password</span>
-                            <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Confirm Password" />
-                        </label>
-
                         <!-- You should use a button here, as the anchor is only used for the example  -->
                         <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
-                            Save password
+                            Verify Account
                         </a>
                         <hr class="my-8" />
 
