939b1d283ae889e7ecf8330b5220084598ee2be6
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-29 17:37:53 +0200

    ft(Activate): added header and page title

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -29,7 +29,7 @@
                 <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                     <div class="w-full">
                         <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-                            Reset password
+                            Activate Account
                         </h1>
                         <label class="block text-sm">
                             <span class="text-gray-700 dark:text-gray-400">New Password</span>
