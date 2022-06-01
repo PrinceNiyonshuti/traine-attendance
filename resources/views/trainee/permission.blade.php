20b0cdce98b3d374ee18ed368a508180944a8cc1
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-01 10:59:20 +0200

    ft(Permissions): changed table data title for the all permission

--- resources/views/trainee/permission.blade.php
+++ resources/views/trainee/permission.blade.php
@@ -8,7 +8,7 @@
                 <table class="w-full whitespace-no-wrap">
                     <thead>
                         <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                            <th class="px-4 py-3">Permissions</th>
+                            <th class="px-4 py-3">All Permissions</th>
                             <th class="px-4 py-3">Actions</th>
                             <th class="px-4 py-3">Status</th>
                             <th class="px-4 py-3">Date</th>
