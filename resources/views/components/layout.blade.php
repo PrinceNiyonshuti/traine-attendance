33961dd67158900589468b2d71c76f3b004c8c4e
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-03 21:52:00 +0200

    ch(Hidding): hidded Attendance attributes that should be hidden for serialization.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -83,7 +83,7 @@
                 </ul>
                 <div class="px-6 my-6">
                     <button class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                        Start Attendance
+                        Attendance
                         <span class="ml-2" aria-hidden="true">+</span>
                     </button>
                 </div>
