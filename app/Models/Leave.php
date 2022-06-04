46f38ef69a985b370814bb3777b033bc6ff0c1e9
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-04 10:31:28 +0200

    ft(Casting): casted Leave attributes that should be casted

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -15,4 +15,11 @@ class Leave extends Model
      * @var array<int, string>
      */
     protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
 }
