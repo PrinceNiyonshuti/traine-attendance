c700786a43aa8d0958a5f37b1237cbb58fbef344
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-04 10:38:34 +0200

    ft(Trainee): added HasApiTokens sanctum

--- app/Models/Trainee.php
+++ app/Models/Trainee.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Trainee extends Authenticatable
 {
