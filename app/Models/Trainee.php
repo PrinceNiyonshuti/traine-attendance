66d6d82e23c4c51e24d138b58573cfddad9f0cc3
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-04 10:36:24 +0200

    ft(Trainee): extended the authenticable illuminate

--- app/Models/Trainee.php
+++ app/Models/Trainee.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Trainee extends Model
+class Trainee extends Authenticatable
 {
     use HasFactory;
 
