2d08e7686ed5c613a94245dcaaf3e99d68568bf5
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-05 20:39:43 +0200

    ft(Report): updated reports title

--- resources/views/mentor/report.blade.php
+++ resources/views/mentor/report.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="container px-6 mx-auto grid">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Daily Report
+            Daily Reports
         </h2>
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
