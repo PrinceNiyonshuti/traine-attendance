445e98640f55e26d4ed8a4ac1dc12ebc60b86ff7
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-05 20:29:04 +0200

    ft(Migration): adding columns to Attendance migration

--- database/migrations/2022_05_24_161745_create_attendances_table.php
+++ database/migrations/2022_05_24_161745_create_attendances_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('trainee_id');
             $table->timestamps();
         });
     }
