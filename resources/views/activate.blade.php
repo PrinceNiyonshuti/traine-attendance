88a16e2dfb98a6c1f2b03b73b0da2e080261cbe3
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-08 16:13:20 +0200

    ch(form): added form attribute to activate account view

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -19,34 +19,36 @@
 </head>
 
 <body>
-    <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-        <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-            <div class="flex flex-col overflow-y-auto md:flex-row">
-                <div class="h-32 md:h-auto md:w-1/2">
-                    <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../assets/img/create-account-office.jpeg" alt="Office" />
-                    <img aria-hidden="true" class="hidden object-cover w-full h-full dark:block" src="../assets/img/create-account-office-dark.jpeg" alt="Office" />
-                </div>
-                <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-                    <div class="w-full">
-                        <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-                            Activate Account
-                        </h1>
-                        <!-- You should use a button here, as the anchor is only used for the example  -->
-                        <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
-                            Verify Account
-                        </button>
-                        <hr class="my-8" />
+    <form>
+        <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+            <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                <div class="flex flex-col overflow-y-auto md:flex-row">
+                    <div class="h-32 md:h-auto md:w-1/2">
+                        <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../assets/img/create-account-office.jpeg" alt="Office" />
+                        <img aria-hidden="true" class="hidden object-cover w-full h-full dark:block" src="../assets/img/create-account-office-dark.jpeg" alt="Office" />
+                    </div>
+                    <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                        <div class="w-full">
+                            <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+                                Activate Account
+                            </h1>
+                            <!-- You should use a button here, as the anchor is only used for the example  -->
+                            <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
+                                Verify Account
+                            </button>
+                            <hr class="my-8" />
 
-                        <p class="mt-4">
-                            <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="/">
-                                Go back to login
-                            </a>
-                        </p>
+                            <p class="mt-4">
+                                <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="/">
+                                    Go back to login
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </body>
 
 </html>
