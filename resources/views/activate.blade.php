3527dc3391637454dc805ecc4341a7c3f4f140e7
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-08 16:12:58 +0200

    ch(activet): on activate account changed link to button

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -32,9 +32,9 @@
                             Activate Account
                         </h1>
                         <!-- You should use a button here, as the anchor is only used for the example  -->
-                        <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
+                        <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
                             Verify Account
-                        </a>
+                        </button>
                         <hr class="my-8" />
 
                         <p class="mt-4">
