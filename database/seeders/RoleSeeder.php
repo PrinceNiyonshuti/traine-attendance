b9af8d62ecc6c73db7398139403a47664cc4d0cd
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-12 19:09:25 +0200

    ch(Seeder): added schema facades to be used

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class RoleSeeder extends Seeder
 {
