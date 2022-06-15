678cd60b83e64cf9334aacb29b28290da9fbb0c6
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-15 17:31:49 +0200

    ch(forgot): changed the link to a button

--- resources/views/forgot.blade.php
+++ resources/views/forgot.blade.php
@@ -42,9 +42,9 @@
                             </label>
 
                             <!-- You should use a button here, as the anchor is only used for the example  -->
-                            <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/reset">
+                            <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/reset">
                                 Recover password
-                            </a>
+                            </button>
                             <hr class="my-8" />
 
                             <p class="mt-4">
