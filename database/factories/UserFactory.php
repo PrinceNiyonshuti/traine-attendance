7deca4f355dde3b375394d7ff059f4f3c8b58559
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-17 17:57:30 +0200

    ch(factory): update role id in user factory with factory reolationshiop

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -19,6 +20,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => "Super admin",
+            'role_id' => Role::all()->random()->id,
             'email' => "admin@gmail.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
