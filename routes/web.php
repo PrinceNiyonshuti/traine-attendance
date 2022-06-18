e3006cdb6175d0df8985ee1a46ffcacc263fafd1
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-18 13:00:12 +0200

    ch(Auth): create login route

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
-// Trainee registration
-Route::get('/login', [TraineeController::class, 'create'])->middleware('guest');
+
+// Authentication
+Route::get('/', [TraineeController::class, 'create'])->middleware('guest');
+Route::post('/login', [TraineeController::class, 'store'])->middleware('guest');
 
 // Trainee registration
 Route::get('/register', [TraineeController::class, 'create'])->middleware('guest');
