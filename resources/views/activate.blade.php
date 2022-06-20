2cc6850239fbf6fe55c188b37b7ef66e9c7b1d76
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-20 06:53:23 +0200

    ch(comments): removed the comments on activate account view

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -32,7 +32,6 @@
                             <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                                 Activate Account
                             </h1>
-                            <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
                                 Verify Account
                             </button>
