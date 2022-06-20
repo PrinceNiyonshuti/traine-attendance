a42ae6aabe65e7adc14c296c871484d3ca54881b
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-20 06:52:37 +0200

    ch(comments): removed the comments on login view

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -63,8 +63,6 @@
                                     </span>
                                 </label>
                             </div>
-
-                            <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                 Create account
                             </button>
