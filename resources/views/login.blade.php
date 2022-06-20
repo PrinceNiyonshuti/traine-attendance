3cb91aeced59abd87fc03f1b4389935cd6a8a975
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-20 06:52:20 +0200

    ch(comments): removed the comments on register view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,8 +47,6 @@
                                 <p class=" text-xs mt-1" style="color: red;">{{ $message }}</p>
                                 @enderror
                             </label>
-
-                            <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
                                 Log in
                             </button>
