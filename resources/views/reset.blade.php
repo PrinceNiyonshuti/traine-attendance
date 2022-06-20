42afc7440b825b1dd7c16bcd9306632fb0ce495a
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-20 06:52:50 +0200

    ch(comments): removed the comments on reset view

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -46,8 +46,6 @@
                                 </span>
                                 <input type="password" name="confirm-password" id="confirm-password" placeholder="***************" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" />
                             </label>
-
-                            <!-- You should use a button here, as the anchor is only used for the example  -->
                             <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" href="/mentor">
                                 Save password
                             </a>
