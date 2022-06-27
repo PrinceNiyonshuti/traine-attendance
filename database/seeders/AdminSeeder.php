ae935575ac5591e7dae4d84bb23a2035066c7540
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-27 12:35:45 +0200

    ft(Seeder): update truncate in admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,7 +19,7 @@ class AdminSeeder extends Seeder
     {
         //
         Schema::disableForeignKeyConstraints();
-        User::truncate();
+        // User::truncate();
         User::create(['username' => 'Super admin', 'role_id' => '1', 'email' => 'admin@gmail.com', 'email_verified_at' => now(), 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'remember_token' => Str::random(10)]);
         Schema::enableForeignKeyConstraints();
     }
