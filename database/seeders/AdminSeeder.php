2363b910402fed4c4bc70dfb64e147b858842207
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-27 12:28:25 +0200

    ft(Seeder): created admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        User::create(['username' => 'Super admin', 'role_id' => '1', 'email' => 'admin@gmail.com', 'email_verified_at' => now(), 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'remember_token' => Str::random(10)]);
+        Schema::enableForeignKeyConstraints();
+    }
+}
