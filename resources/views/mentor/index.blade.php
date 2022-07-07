dbcbfc56074978ca733c9e1856519edc68f17f4f
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-07 16:06:11 +0200

    ft(dashboard): added trainee cards of permissions

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -100,7 +100,7 @@
                 </div>
                 <div>
                     <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        Active Trainees
+                        Permissions
                     </p>
                     <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                         $ 46,760.89
