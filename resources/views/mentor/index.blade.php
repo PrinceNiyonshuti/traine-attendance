e41ac86ad522256d55c558b5cd6fd3f2fba145ec
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-07 16:06:36 +0200

    ft(dashboard): added trainee cards of missed attendance

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -116,7 +116,7 @@
                 </div>
                 <div>
                     <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        New Trainees
+                        Missed Attendance
                     </p>
                     <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                         376
