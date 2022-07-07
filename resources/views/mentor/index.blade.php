6e27313002a8b46822067b617f40e4d74c64fb77
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-07 16:14:27 +0200

    ft(dashboard): added mentor cards of permissions

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -63,7 +63,7 @@
                 </div>
                 <div>
                     <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                        Pending Trainees
+                        Permissions
                     </p>
                     <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                         35
