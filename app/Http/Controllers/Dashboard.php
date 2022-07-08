a4e907d7ec439372a0817cdf21dc801eff792b2b
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-08 17:45:38 +0200

    ft(Dashboard): added trainees analytics data

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Dashboard extends Controller
 {
     //
-    public function dashbooard()
+    public function dashboard()
     {
         # code...
         if (auth()->user()?->level_id == '1') {
             # code...
-            return view('/mentor/index');
+            $trainees = User::where('level_id', '=', '2')->count();
+            $analytics = [$trainees];
+            return view(
+                '/mentor/index',
+                ["analytics" => $analytics]
+            );
         } else {
             # code...
             return view('/mentor/index');
