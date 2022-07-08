83a5dfd21cea3a550e3d45566fc74020b1b74090
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-08 17:27:17 +0200

    ch(dashboard): fixed the dashboard username on mentor

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="container px-6 mx-auto grid">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Dashboard {{ Auth::user()->username }}
+            Dashboard 
         </h2>
         @can('mentor')
         <!-- Mentor Cards -->
