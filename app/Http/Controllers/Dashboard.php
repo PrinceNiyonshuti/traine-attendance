63a1ea52634e22896dabf198418a332e7bc339c4
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-08 17:32:38 +0200

    ft(Dashboard): added dashboard function

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class Dashboard extends Controller
 {
     //
+    public function dashbooard()
+    {
+        # code...
+        if (auth()->user()?->level_id == '1') {
+            # code...
+            return view('dashboard.dashboard');
+        } else {
+            # code...
+            return view('dashboard.dashboard_trainee');
+        }
+    }
 }
