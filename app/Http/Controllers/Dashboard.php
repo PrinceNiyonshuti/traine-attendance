c50cd82cf3e9bed1a567d5cc6ef14ed8f3bf07ce
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-09 20:29:08 +0200

    fx(analytics): fixed permissions function for count

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LeavePermission;
 use App\Models\Trainee;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class Dashboard extends Controller
         $trainees = Trainee::count();
         $activeTrainees = Trainee::where('status','=','1')->count();
         $attendance = User::where('role_id','=','2')->count();
-        $permissions = User::where('role_id','=','2')->count();
+        $permissions = LeavePermission::where('leaveStatus','=','1')->count();
         $analytics = [$trainees,$activeTrainees,$attendance,$permissions];
         return view(
             '/mentor/index',
