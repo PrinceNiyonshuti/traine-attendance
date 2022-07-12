4e9c51385c69d3f012459b6129f57bd0f18dc456
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-12 19:59:26 +0200

    ft(Networks): added networks address  to table

--- resources/views/mentor/networks.blade.php
+++ resources/views/mentor/networks.blade.php
@@ -22,6 +22,9 @@
                             <td class="px-4 py-3 ">
                                 {{ $network->networkName }}
                             </td>
+                            <td class="px-4 py-3 ">
+                                {{ $network->networkAddress }}
+                            </td>
                             <td class="px-4 py-3 text-xs">
                                 @if ($network->networkStatus == 1)
                                 <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
