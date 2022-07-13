1d83d93284e3d268c3373c76343b1882b5e12294
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-13 07:35:14 +0200

    fx(Networks): changed ip address to network address

--- resources/views/mentor/networks.blade.php
+++ resources/views/mentor/networks.blade.php
@@ -10,7 +10,7 @@
                     <thead>
                         <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                             <th class="px-4 py-3">Name</th>
-                            <th class="px-4 py-3">IP Address</th>
+                            <th class="px-4 py-3">Network Address</th>
                             <th class="px-4 py-3">Status</th>
                             <th class="px-4 py-3">Date</th>
                             <th class="px-4 py-3">Actions</th>
