2014d08cd84018f4818ae6d81ebfddbad48f14e4
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-13 07:37:23 +0200

    fx(Mentor): removed empty spaces

--- resources/views/mentor/index.blade.php
+++ resources/views/mentor/index.blade.php
@@ -142,8 +142,6 @@
         </div>
         <!-- Trainee Cards -->
         @endcan
-
-
         <!-- New Table -->
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
