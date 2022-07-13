88b2123b4b7e7df0ff6f83f9fb0e0998d380e9cb
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-13 07:36:31 +0200

    fx(Networks): removed empty spaces

--- resources/views/mentor/networks.blade.php
+++ resources/views/mentor/networks.blade.php
@@ -3,7 +3,6 @@
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
             Networks
         </h2>
-
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
                 <table class="w-full whitespace-no-wrap">
