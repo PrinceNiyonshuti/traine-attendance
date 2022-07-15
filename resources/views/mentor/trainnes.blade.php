20e7f71ce16482867e5e62c5bf52cddc3c3600d9
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-15 17:27:36 +0200

    ft(pagination): added pagination to trainee view

--- resources/views/mentor/trainnes.blade.php
+++ resources/views/mentor/trainnes.blade.php
@@ -70,6 +70,7 @@
                     </tbody>
                 </table>
             </div>
+            {{ $trainees->links() }}
             <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
                 <span class="flex items-center col-span-3">
                     Showing 21-30 of 100
