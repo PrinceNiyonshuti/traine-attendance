349f6bf762dc556854449188cf66320f6d0f24db
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-04 11:35:11 +0200

    added form action and method to reset form

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-    <form>
+    <form method="POST" action="#">
         @csrf
         <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
             <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
