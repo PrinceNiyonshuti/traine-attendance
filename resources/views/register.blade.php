b131dbede06d9efae0a343c278b4da9b994a6f6f
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-18 15:19:59 +0200

    (ft-app-logo): added app logo icon to register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Trainee Attendance :: Register</title>
+    <link href="{{ asset('icon.png') }}" rel="icon">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
     <!-- Fonts -->
