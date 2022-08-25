0a19349753aa7d3aef24a6713c837d4dea482939
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-25 20:36:29 +0200

    (ft-trainee-sidebar): added traineeSettings to the sidebar

--- resources/views/components/traineeSideBar.blade.php
+++ resources/views/components/traineeSideBar.blade.php
@@ -34,7 +34,7 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="/mentorSettings">
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="/traineeSettings">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
                     </svg>
