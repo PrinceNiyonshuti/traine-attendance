ff7508a0e46da0490536366336f06ab083c1fedf
PrinceNiyonshuti <npprince47@gmail.com>
2022-10-18 22:51:23 +0200

    (ft-forgot-password): added reset email template

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
---
+++ resources/views/emails/reset-password-email.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+# Hello!
+
+You are receiving this email because we received a password reset request for your account.
+
+Your Password Reset code is : {{$activationCode}}
+
+@component('mail::button', ['url' => route('password.reset' ,$token)])
+Reset Password
+@endcomponent
+
+This password reset link will expire in 60 minutes.
+
+If you did not request a password reset, no further action is required.
+
+Regards,<br>
+{{ config('app.name') }}
+
+<hr>
+<font size="2">If you’re having trouble clicking the "Reset Password" button, copy and paste the URL below into your web browser: <a href="{{route('password.reset',$token)}}">{{route('password.reset',$token)}}</a></font>
+@endcomponent
